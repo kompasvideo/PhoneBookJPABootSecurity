bb2c80b931199adf50130d494b83d21d3efb24d5
Андрей Барышников <andrey1andrey@narod.ru>
2023-08-21 20:51:02 +0300

    1

---
+++ src/main/java/ru/andreybaryshnikov/phonebookjpabootsecurity/PhoneBookJpaBootSecurityApplication.java
@@ -0,0 +1,13 @@
+package ru.andreybaryshnikov.phonebookjpabootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoneBookJpaBootSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhoneBookJpaBootSecurityApplication.class, args);
+    }
+
+}
